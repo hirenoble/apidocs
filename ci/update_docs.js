ebe1ecb33a2ce96d2f721e1790f150932629eece
Hiren Anand Shah <hirenanandshah@outlook.com>
2019-05-15 14:23:43 -0600

    Update update_docs.js

--- ci/update_docs.js
+++ ci/update_docs.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const collection = require('../postman_collection.json')
 var apiKey = '8ef445f78b0c419db6bfdc92d3225a56';
-var collectionId = '2147941-fc12cf90-7c58-4964-ad32-9d7ff6a49893';
+var collectionId = '2147941-8f4bebf2-3e0d-4128-b066-b35ed782ee76';
 
 updateDocs()
 
