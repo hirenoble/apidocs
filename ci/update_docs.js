44221dba9d1a4fb00739ac9652a9789c9c448804
Hiren Anand Shah <hirenanandshah@outlook.com>
2019-06-03 16:21:33 -0600

    Update update_docs.js

--- ci/update_docs.js
+++ ci/update_docs.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
-const collection = require('../postman_collection.json')
-var apiKey = '8ef445f78b0c419db6bfdc92d3225a56';
-var collectionId = '2147941-8f4bebf2-3e0d-4128-b066-b35ed782ee76';
+const collection = require('../stock.json')
+var token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdG9wbGlnaHR8MjA4NzEiLCJ1c2VybmFtZSI6ImhpcmVuc2hhaDExMSIsImVtYWlsIjoiaGlyZW5hbmFuZHNoYWhAb3V0bG9vay5jb20iLCJuYW1lIjoiSGlyZW4gU2hhaCIsImlhdCI6MTU1OTMzMzU3MCwia2V5Ijoic0dDalRIcXFYTUVzNDZwSkNDNkwifQ.yssNRz7nmUwLw2QhlVVNuort8jpTV8GiGxqdoyS-tGo";
+
 
 updateDocs()
 
@@ -9,15 +9,15 @@ async function updateDocs () {
   try {
     const update = await axios.request({
       method: 'put',
-      headers: { 'X-Api-Key' : apiKey, 'Content-Type' : 'application/json' },
-      url: `https://api.getpostman.com/collections/${collectionId}`,
-      data: { collection : collection }
+      headers: { 'Authorization' : 'Bearer '+token, 'Content-Type' : 'application/json' },
+      url: `https://next-api.stoplight.io/projects/23747/files/stock.oas2.yml`,
+      data: { content : collection, branch : 'version 1.0', commit_message :  'updating stock api'}
     })
 
     if (update) {
-      console.log(`Postman collection ${collection.info.name} updated.`)
+      console.log(`StopLight file updated.`)
     } else {
-      console.log('Updating Postman collection failed!')
+      console.log('Updating stoplight collection failed!')
     }
   } catch (e) {
     console.log(e.response.data)
